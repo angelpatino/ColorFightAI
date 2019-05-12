b6cb1831f1d1ec34163c4e36872f28b512e80170
angelpatino <49142557+angelpatino@users.noreply.github.com>
2019-05-11 19:24:57 -0700

    Update ColorGnoc.py

--- ColorGnoc.py
+++ ColorGnoc.py
@@ -15,14 +15,12 @@ ring = 1
 is_first_move = True
 continue_moving = True
 ring_finished = False
-game.connect(room = 'nopublic')
+is_stopping = False
+moved_this_side = 0
+game.connect(room = 'public1')
 
-if game.register(username = 'AngelP123' + str(random.randint(1, 100)), \
+if game.register(username = 'AngelP123', \
         password = 'YeeHaw'):
-
-
-
-
     while True:
         cmd_list = []
         my_attack_list = []
@@ -30,21 +28,30 @@ if game.register(username = 'AngelP123' + str(random.randint(1, 100)), \
         if game.me == None:
             continue
         me = game.me
-        if is_first_move:
-            print(len(game.me.cells))
-            for cell in game.me.cells.values():
-                home = cell.position
-                print("home: ", home.x, home.y)
-                position = home
 
+        while True:
+
+            if is_first_move:
+                print(len(game.me.cells))
+                for cell in game.me.cells.values():
+                    home = cell.position
+                    print("home: ", home.x, home.y)
+                    position = home
+            
+                position.y = position.y+1
+                c = game.game_map[position]
+                print("Checking cell ({}, {})".format(position.x, position.y))
+                if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
+                    cmd_list.append(game.attack(position, c.attack_cost))
+                    print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
+                    game.me.energy -= c.attack_cost
+                    my_attack_list.append(c.position)
                 is_first_move = False
-
-        while continue_moving:
             
             if side == TOP:
                 for i in range(ring - moved_this_side):
                     position.x = position.x-1
-                    if (position.x >= 0 and position.x < game.game_map.width and position.y >= 0 and position.y < game.game_map.height):
+                    if (position.x >= 0 and position.x < 30 and position.y >= 0 and position.y < 30):
                         c = game.game_map[position]
                         print("Checking cell ({}, {})".format(position.x, position.y))
                         if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
@@ -53,47 +60,25 @@ if game.register(username = 'AngelP123' + str(random.randint(1, 100)), \
                             game.me.energy -= c.attack_cost
                             my_attack_list.append(c.position)
                         else:
-                            # save everything
-                            moved_this_side = i
+                            moved_this_side = i + 1
                             is_stopping = True
                             break
+                    if is_stopping:
+                        moved_this_side = 0
+                        break
                 if is_stopping:
+                    is_stopping = False
+                    moved_this_side = 0
                     break           
                 
-                if ring_finished:
-                    position.y = position.y + 1
-                    ring_finished = False
-                    ring += 1 
-                    for i in range(ring):
-                        position.x = position.x-1
-                        if (position.x >= 0 and position.x < game.game_map.width and position.y >= 0 and position.y < game.game_map.height):
-                            # print("left", position.x, position.y)
-                            c = game.game_map[position]
-                            print("Checking cell ({}, {})".format(position.x, position.y))
-                            if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
-                                cmd_list.append(game.attack(position, c.attack_cost))
-                                print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
-                                game.me.energy -= c.attack_cost
-                                my_attack_list.append(c.position)
-                side = LEFT
-            
-            if side == LEFT:
-                for i in range(2*ring):
-                    position.y = position.y-1
-                    if (position.x >= 0 and position.x < game.game_map.width and position.y >= 0 and position.y < game.game_map.height):
-                        # print("down", position.x, position.y)
-                        c = game.game_map[position]
-                        if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
-                            cmd_list.append(game.attack(position, c.attack_cost))
-                            print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
-                            game.me.energy -= c.attack_cost
-                            my_attack_list.append(c.position)
-                side = BOTTOM
-
-            if side == BOTTOM:
-                for i in range(2*ring):
-                    position.x = position.x+1
-                    if (position.x >= 0 and position.x < game.game_map.width and position.y >= 0 and position.y < game.game_map.height):
+            if ring_finished:
+                position.y = position.y + 1
+                ring_finished = False
+                ring += 1 
+                for i in range(ring):
+                    position.x = position.x-1
+                    if (position.x >= 0 and position.x < 30 and position.y >= 0 and position.y < 30):
+                        # print("left", position.x, position.y)
                         c = game.game_map[position]
                         print("Checking cell ({}, {})".format(position.x, position.y))
                         if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
@@ -101,47 +86,110 @@ if game.register(username = 'AngelP123' + str(random.randint(1, 100)), \
                             print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
                             game.me.energy -= c.attack_cost
                             my_attack_list.append(c.position)
-                side = RIGHT
-
-            if side == RIGHT:
+                        else:
+                            moved_this_side = i + 1
+                            is_stopping = True
+                            break
+                    if is_stopping:
+                        moved_this_side = 0
+                        break
+                if is_stopping:
+                    is_stopping = False
+                    moved_this_side = 0
+                    break       
+                side = LEFT
+            
+            if side == LEFT:
                 for i in range(2*ring):
-                    position.y = position.y+1
-                    if (position.x >= 0 and position.x < game.game_map.width and position.y >= 0 and position.y < game.game_map.height):
+                    position.y = position.y-1
+                    if (position.x >= 0 and position.x < 30 and position.y >= 0 and position.y < 30):
+                        # print("down", position.x, position.y)
                         c = game.game_map[position]
                         if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
                             cmd_list.append(game.attack(position, c.attack_cost))
                             print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
                             game.me.energy -= c.attack_cost
                             my_attack_list.append(c.position)
+                        else:
+                            moved_this_side = i + 1
+                            is_stopping = True
+                            break
+                    if is_stopping:
+                        moved_this_side = 0
+                        break
+                if is_stopping:
+                    is_stopping = False
+                    moved_this_side = 0
+                    break
+                side = BOTTOM
+
+            if side == BOTTOM:
+                for i in range(2*ring):
+                    position.x = position.x+1
+                    if (position.x >= 0 and position.x < 30 and position.y >= 0 and position.y < 30):
+                        c = game.game_map[position]
+                        print("Checking cell ({}, {})".format(position.x, position.y))
+                        if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
+                            cmd_list.append(game.attack(position, c.attack_cost))
+                            print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
+                            game.me.energy -= c.attack_cost
+                            my_attack_list.append(c.position)
+                        else:
+                            moved_this_side = i + 1
+                            is_stopping = True
+                            break
+                    if is_stopping:
+                        moved_this_side = 0
+                        break
+                if is_stopping:
+                    is_stopping = False
+                    moved_this_side = 0
+                    break
+                side = RIGHT
+
+            if side == RIGHT:
+                for i in range(2*ring):
+                    position.y = position.y+1
+                    if (position.x >= 0 and position.x < 30 and position.y >= 0 and position.y < 30):
+                        c = game.game_map[position]
+                        if c.attack_cost < me.energy and c.owner != game.uid and me.energy > MIN_ENERGY:
+                            cmd_list.append(game.attack(position, c.attack_cost))
+                            print("We are attacking ({}, {}) with {} energy".format(position.x, position.y, c.attack_cost))
+                            game.me.energy -= c.attack_cost
+                            my_attack_list.append(c.position)
+                        else:
+                            moved_this_side = i + 1
+                            is_stopping = True
+                            break
+                    if is_stopping:
+                        moved_this_side = 0
+                        break
+                if is_stopping:
+                    is_stopping = False
+                    moved_this_side = 0
+                    break 
                 side = TOP
                 ring_finished = True
 
-        for cell in game.me.cells.values():
-            # Check the surrounding position
-            for pos in cell.position.get_surrounding_cardinals():
-                c = game.game_map[pos] 
-                if cell.building.can_upgrade and \
-                    (cell.building.is_home or cell.building.level < me.tech_level) and \
-                    cell.building.upgrade_gold < me.gold and \
-                    cell.building.upgrade_energy < me.energy:
-                    cmd_list.append(game.upgrade(cell.position))
-                    print("We upgraded ({}, {})".format(cell.position.x, cell.position.y))
-                    me.gold   -= cell.building.upgrade_gold
-                    me.energy -= cell.building.upgrade_energy
-                
-                # Build a random building if we have enough gold
-                if cell.owner == me.uid and cell.building.is_empty and me.gold >= 100:
-                    building = random.choice([BLD_FORTRESS, BLD_GOLD_MINE, BLD_ENERGY_WELL])
-                    cmd_list.append(game.build(cell.position, building))
-                    print("We build {} on ({}, {})".format(building, cell.position.x, cell.position.y))
-                    me.gold -= 100
-            result = game.send_cmd(cmd_list)
-            print(result)
+            if cell.building.can_upgrade and \
+                (cell.building.is_home or cell.building.level < me.tech_level) and \
+                cell.building.upgrade_gold < me.gold and \
+                cell.building.upgrade_energy < me.energy:
+                cmd_list.append(game.upgrade(cell.position))
+                print("We upgraded ({}, {})".format(cell.position.x, cell.position.y))
+                me.gold   -= cell.building.upgrade_gold
+                me.energy -= cell.building.upgrade_energy
+            
+            # Build a random building if we have enough gold
+            if cell.owner == me.uid and cell.building.is_empty and me.gold >= 100:
+                building = random.choice([BLD_FORTRESS, BLD_GOLD_MINE, BLD_ENERGY_WELL])
+                cmd_list.append(game.build(cell.position, building))
+                print("We build {} on ({}, {})".format(building, cell.position.x, cell.position.y))
+                me.gold -= 100
+            
+        result = game.send_cmd(cmd_list)
+        print(result)
 
-
-
-
-        # act from current position
 
  
     
